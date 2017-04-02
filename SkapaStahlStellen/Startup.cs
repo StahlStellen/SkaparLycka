c993ea8140309200aad016fe0b3c7c45fefbc582
list1501 <list1501@student.miun.se>
2017-04-02 19:17:52 +0200

    Projectinitial

---
+++ SkapaStahlStellen/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkapaStahlStellen.Startup))]
+namespace SkapaStahlStellen
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
